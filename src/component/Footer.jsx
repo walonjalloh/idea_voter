fbdef6cea65c55e0b3c165a63ca2ba1d66658af0
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-08-23 04:26:30 +0000

    commit 2

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
 
   const year = new Date().getFullYear()
   return (
-    <footer className="flex justify-center ">
-      <p>All Right Reserved Walon &copy;{year}</p>
+    <footer className="flex justify-center sticky bottom-0 z-0">
+      <p className="font-semibold text-2xl my-10 ">All Right Reserved Walon &copy;{year}</p>
     </footer>
   )
 }
