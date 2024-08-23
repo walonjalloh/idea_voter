4bfaf23b712892ae0e4282ef18bd1bcc35972540
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-08-23 01:15:58 +0000

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Footer, Navbar, Page } from "./component/component"
+
+
+function App() {
+  return (
+    <div className="relative container mx-auto my-auto h-screen">
+      <Navbar/>
+      <Page/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/component.js
@@ -0,0 +1,9 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Page from '../pages/Page';
+
+export {
+    Footer,
+    Navbar,
+    Page
+};
