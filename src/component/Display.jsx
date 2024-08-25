e6f45a0035ace109e4b796b695515b5d260928e2
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-08-25 03:14:42 +0000

    add proptypes needed value data

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -7,7 +7,6 @@ function Display({ card, save }) {
 
   return (
     <div className="flex flex-col flex-1  mx-2 rounded-xl border-black border-[4px]  py-4 my-4">
-      
          <div className="flex flex-col justify-center items-center">
          <label className="text-2xl font-bold ">Title</label>
          <h2 className="font-normal font-mono text-2xl my-2 text-center">{card.title}</h2>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
 
   const year = new Date().getFullYear()
   return (
-    <footer className="flex justify-center">
-      <p className="font-semibold md:text-2xl my-5 ">All Right Reserved &copy;{year}</p>
+    <footer className="flex justify-center left-0  w-full bg-black sticky bottom-0 ">
+      <p className="font-semibold text-[20px] md:text-2xl my-5 text-white ">All Right Reserved &copy;{year}</p>
     </footer>
   )
 }
