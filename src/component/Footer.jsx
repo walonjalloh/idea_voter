a8171dbc5abe564a8c45bbcc66aa830d6af062d9
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-08-25 22:02:38 +0000

    a showcase for idea

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
 
   const year = new Date().getFullYear()
   return (
-    <footer className="flex justify-center left-0  w-full bg-black sticky bottom-0 ">
-      <p className="font-semibold text-[20px] md:text-2xl my-5 text-white ">All Right Reserved &copy;{year}</p>
+    <footer className="flex justify-center left-0  w-full  sticky bottom-0 ">
+      <p className="font-semibold text-[20px] md:text-2xl my-5 text-black ">All Right Reserved &copy;{year}</p>
     </footer>
   )
 }
