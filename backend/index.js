399620e848f61704f3d3ceb03d55587ed550c770
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-10-05 21:49:22 +0000

    add sign in and sign up api in the index.js file

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import connectDB from './configs/mongoDB.js'
 import corsOptions from './configs/cors.js'
 import User from './models/userSchema.js'
 import Content from './models/contentSchema.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const PORT = 3500
 const app = express()
@@ -69,7 +71,20 @@ app.post('/ideas', async(req,res) => {
    }
 })
 
+//deleting an idea 
+app.delete('/ideas/:id', async(req,res) => {
+    const { id } = req.params
 
+    try{
+        const idea = await Content.findByIdAndDelete(id)
+        if(!idea){
+            return res.status(500).json({message: 'Invalid'})
+        }
+        res.send(idea)
+    }catch(error){
+        res.status(500).json({message: 'Error in deleting'})
+    }
+})
 
 //getting a specific idea
 app.get('/ideas/:id',async(req,res) => {
@@ -81,6 +96,77 @@ app.get('/ideas/:id',async(req,res) => {
         res.status(500).json({error})
     }
 })
+
+
+
+//user signup
+app.post('/user/signup', async(req,res) => {
+    const { fullName, username,password } = req.body
+    try {
+        //validating all the fields
+        if(!fullName || !username || !password){
+            return console.log('All fields are required')
+        }
+
+        //checking if a user exist
+        const userExist = await User.findOne({username})
+        if(userExist){
+            return res.status(404).json({message: 'User with this email exist'})
+        }
+
+        const passwordHashed = await bcrypt.hash(password,10)
+
+        const user = new User({
+            fullName,
+            username,
+            password:passwordHashed
+        })
+        await user.save()
+        const token = jwt.sign({_id:user._id.toString()}, process.env.JWT_SECRET)
+
+        const userResponse = user.toObject()
+        delete userResponse.password
+        
+        res.status(201).json({user: userResponse,token})
+
+    }catch(error){
+        console.log(`Sign up error ${error}`)
+        res.status(500).json({message: 'Error occured during signup'})
+    }
+})
+
+//user sigin 
+app.post('/user/signin', async(req,res) => {
+    const { username, password } = req.body
+    try{
+        //validating all the fields
+        if(!username || !password){
+            return console.log('All field are required')
+        }
+        
+        const user = User.findOne({ username })
+        if(!user){
+            return res.status(500).json({message:"Login failed"})
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if(!passwordMatch){
+            return res.status(500).json({message: 'Login failed'})
+        }
+        
+        const token = jwt.sign({_id:user_id.toString()},process.env.JWT_SECRET)
+        const userResponse = user.toObject()
+        delete userResponse.password
+        res.status(200).json({user: userResponse,token})
+    }catch(error){
+        console.log(error)
+        res.status(400).json(error)
+    }
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
